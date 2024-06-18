c2a50f35a24d68a160dd45521e4149c9499299c4
AkylaiJumabekova <jumabekovaakylai7@gmail.com>
2024-06-18 15:12:22 +0600

    tested if styles are working, created a window where i will put messages inside

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-success d-flex align-items-center justify-content-center min-vh-100">
+      <div className="container mt-5 p-3 bg-light text-dark">
+        Chat Application
+      </div>
+    </div>
+  );
+};
+
+export default App;
